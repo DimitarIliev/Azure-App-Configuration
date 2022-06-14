43248fe2906addd679236255545add20a6fede05
diki_ <diki_@DESKTOP-M5N51NC>
2022-06-14 11:56:32 +0200

    Add project files.

---
+++ AzureAppConfiguration/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
+
+Console.WriteLine("Azure App Configuration Article Demo\n");
+
+//Retrieve the Connection String from Azure App Configuration Resource
+const string connectionString = "{your_app_configuration_connection_string}";
+
+var configuration = new ConfigurationBuilder()
+            .AddAzureAppConfiguration(options =>
+            {
+                options.Connect(connectionString).UseFeatureFlags();
+            }).Build();
+
+var services = new ServiceCollection();
+
+services.AddSingleton<IConfiguration>(configuration).AddFeatureManagement();
+
+using (var serviceProvider = services.BuildServiceProvider())
+{
+    var featureManager = serviceProvider.GetRequiredService<IFeatureManager>();
+
+    //read great day feature
+    if (await featureManager.IsEnabledAsync("greatdayinfo"))
+    {
+        Console.WriteLine("Have a great day!!!");
+    }
+    else
+    {
+        Console.WriteLine("Please enable great day feature!");
+    }
+}
